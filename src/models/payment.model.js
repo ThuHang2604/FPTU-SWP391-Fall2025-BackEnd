557eae6e8449ce861408debb7cd3971beff88eaa
ThuHang2604 <dothuhang2604@gmail.com>
2025-10-27 10:52:28 +0700

    fix payment model

--- src/models/payment.model.js
+++ src/models/payment.model.js
@@ -3,10 +3,6 @@ module.exports = (sequelize, DataTypes) => {
     id: { type: DataTypes.BIGINT, autoIncrement: true, primaryKey: true },
     member_id: { type: DataTypes.BIGINT, allowNull: false },
     amount: { type: DataTypes.DECIMAL(15, 2), allowNull: false },
-    transaction_type: {
-      type: DataTypes.ENUM("TOP_UP", "POST_FEE", "REFUND"),
-      allowNull: false,
-    },
     payment_method: { type: DataTypes.STRING(50), defaultValue: "PAYPAL" },
     payment_status: {
       type: DataTypes.ENUM("PENDING", "COMPLETED", "FAILED"),
