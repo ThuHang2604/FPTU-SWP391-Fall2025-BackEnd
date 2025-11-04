17b112fd0d3d549d6dc4bb2d495bcd2ce9503214
ThuHang2604 <dothuhang2604@gmail.com>
2025-11-04 15:56:46 +0700

    update Db for Google login

--- CreateDBStep2.sql
+++ CreateDBStep2.sql
@@ -7,3 +7,10 @@ ADD COLUMN is_paid TINYINT(1) NOT NULL DEFAULT 0 COMMENT 'Đã thanh toán' AFTE
 
 -- 3) Kiểm tra
 SHOW COLUMNS FROM products LIKE 'is_paid';
+--
+--CHỈNH CỬA DB CHO ĐĂNG NHẬP GOOGLE
+USE ev_trading_platform;
+
+ALTER TABLE users
+ADD COLUMN google_id VARCHAR(255) UNIQUE AFTER email,
+ADD COLUMN login_provider ENUM('LOCAL', 'GOOGLE') DEFAULT 'LOCAL' AFTER password;
