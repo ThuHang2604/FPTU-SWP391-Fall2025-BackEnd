f3be913ac2cde8c3dea3a184206709b3c34970e9
PhanKhanhSE <khanhplse181934@fpt.edu.vn>
2025-11-06 10:03:40 +0700

    Update CreateDBStep2.sql

--- CreateDBStep2.sql
+++ CreateDBStep2.sql
@@ -7,6 +7,13 @@ ADD COLUMN is_paid TINYINT(1) NOT NULL DEFAULT 0 COMMENT 'Đã thanh toán' AFTE
 
 -- 3) Kiểm tra
 SHOW COLUMNS FROM products LIKE 'is_paid';
+--
+--CHỈNH CỬA DB CHO ĐĂNG NHẬP GOOGLE
+USE ev_trading_platform;
+
+ALTER TABLE users
+ADD COLUMN google_id VARCHAR(255) UNIQUE AFTER email,
+ADD COLUMN login_provider ENUM('LOCAL', 'GOOGLE') DEFAULT 'LOCAL' AFTER password;
 
 -- Thêm cột google_id và login_provider cho users
 ALTER TABLE users 
